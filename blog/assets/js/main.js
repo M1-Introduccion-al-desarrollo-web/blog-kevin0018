346f06b6646ae7d7c60b6cdb474813f22e612609
kevin0018 <akevin.2215@gmail.com>
2025-04-28 12:22:13 +0200

    wip - home and coches view

--- blog/assets/js/main.js
+++ blog/assets/js/main.js
@@ -1,41 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Calcular la profundidad de la ruta
-    const depth = window.location.pathname.split('/').length - 2;
-
-    let prefix = '';
-    for (let i = 0; i < depth; i++) {
-        prefix += '../';
-    }
-
     // Cargar el header y el footer
-    loadComponent('header', prefix + 'components/header.html');
-    loadComponent('footer', prefix + 'components/footer.html');
+    loadComponent('header', '../../components/header.html');
+    loadComponent('footer', '../../components/footer.html');
 
-    const countryNameElement = document.getElementById('country-name');
+    // const countryNameElement = document.getElementById('country-name');
 
-    if (countryNameElement) {
-        const path = window.location.pathname.toLowerCase();
 
-        let country = '';
+    // if (countryNameElement) {
+    //     const path = window.location.pathname.toLowerCase();
 
-        // Determinar el nombre del país basándose en la URL
-        if (path.includes('alemania')) {
-            country = 'Alemania';
-        } else if (path.includes('italia')) {
-            country = 'Italia';
-        } else if (path.includes('espana')) {
-            country = 'España';
-        } else {
-            country = ''; // Página principal
-        }
+    //     let country = '';
 
-        // Insertar el nombre del país en el elemento correspondiente
-        countryNameElement.innerHTML = `<b>${country}</b>`;
+    //     // Determinar el nombre del país basándose en la URL
+    //     if (path.includes('alemania')) {
+    //         country = 'Alemania';
+    //     } else if (path.includes('italia')) {
+    //         country = 'Italia';
+    //     } else if (path.includes('grecia')) {
+    //         country = 'Grecia';
+    //     } else {
+    //         country = 'Egipto';
+    //     }
+
+    //     // Insertar el nombre del país en el elemento correspondiente
+    //     console.log(country);
+        // countryNameElement.innerHTML = `<b>${country}</b>`;
 
         // Actualizar el enlace del país
-        const countryLink = document.querySelector('.flag-link');
-        countryLink.href = `${country.toLowerCase()}.html`;
-    }
+        // const countryLink = document.querySelector('.flag-link');
+        // countryLink.href = `${country.toLowerCase()}.html`;
+    // }
 });
 
 // Función para cargar header o footer
